30226cb0968f2382115051ab103989653904a61b
Bobby058 <suryawbobby@gmail.com>
2026-06-21 06:10:11 +0700

    Update test.php

--- public/test.php
+++ public/test.php
@@ -2,7 +2,9 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-$_SERVER['REQUEST_URI'] = '/auth/login';
-$_SERVER['REQUEST_METHOD'] = 'POST';
+require_once __DIR__ . '/../src/db.php';
 
-require_once __DIR__ . '/../src/server.php';
+$db = getDB();
+$stmt = $db->query("SELECT * FROM users LIMIT 1");
+$user = $stmt->fetch();
+echo json_encode($user);
