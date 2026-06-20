85bc82a1900e51bd9baed2f8d6d87cc48e251272
Bobby058 <suryawbobby@gmail.com>
2026-06-21 05:07:55 +0700

    Update test.php

--- public/test.php
+++ public/test.php
@@ -1,2 +1,7 @@
 <?php
-phpinfo();
+echo "PDO drivers: ";
+print_r(PDO::getAvailableDrivers());
+echo "<br>DB_HOST: " . getenv('DB_HOST');
+echo "<br>DB_NAME: " . getenv('DB_NAME');
+echo "<br>DB_USER: " . getenv('DB_USER');
+echo "<br>DB_PASS: " . (getenv('DB_PASSWORD') ? 'ada' : 'kosong');
