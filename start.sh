09b5a3fb84a4d5cd9d6d3f83b350a33282fdc508
Bobby058 <suryawbobby@gmail.com>
2026-06-21 05:02:51 +0700

    Refactor start.sh to use PORT variable

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-sed -i "s/Listen 80/Listen ${PORT:-8080}/" /etc/apache2/ports.conf
-sed -i "s/*:80>/*:${PORT:-8080}>/" /etc/apache2/sites-enabled/000-default.conf
+PORT=${PORT:-8080}
+sed -i "s/Listen 80/Listen $PORT/" /etc/apache2/ports.conf
+sed -i "s/*:80>/*:$PORT>/" /etc/apache2/sites-enabled/000-default.conf
 exec apache2ctl -D FOREGROUND
