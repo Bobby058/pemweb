edced8cb19ac602601326e99fa7a703fd375503f
Bobby058 <suryawbobby@gmail.com>
2026-06-21 06:38:37 +0700

    Enhance start.sh to pass env vars to php-fpm
    Add environment variable passing for php-fpm.

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 PORT=${PORT:-8080}
+
+# Pass env vars ke php-fpm
+printenv | grep -E "^(DB_|JWT_)" | sed 's/^/env[/' | sed 's/=/]="/' | sed 's/$/"/' >> /etc/php/8.2/fpm/pool.d/www.conf
+
 sed -i "s/listen 80/listen $PORT/" /etc/nginx/sites-enabled/default
 php-fpm8.2 -D
 nginx -g 'daemon off;'
